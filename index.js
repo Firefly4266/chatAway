29a3804667d08e82723070e646a2b58cf532a3fc
Firefly4266 <gaines.johnp@gmail.com>
2019-01-15 20:53:37 -0800

    initial build out

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let socket = require('socket.io');
+
+let app = express();
+
+let server = app.listen(4000, function() {
+    console.log('Now listening on port 4000');
+});
+app.use(express.static("Public"));
+
+let io = socket(server);
+
+io.on("connection", function(socket) {
+    console.log('made socket connection.', socket.id);
+});
