e4ef609e4b9bdbdaadbd915b07f2931438d36b36
Firefly4266 <gaines.johnp@gmail.com>
2019-01-16 20:13:39 -0800

    added functionality

--- Public/chatAway.js
+++ Public/chatAway.js
@@ -1,1 +1,17 @@
 let socket = io.connect("http://localhost:4000");
+
+let handle = document.getElementById('handle'),
+    message = document.getElementById('message'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output');
+
+btn.addEventListener('click', function() {
+    socket.emit('chat', {
+        handle: handle.value,
+        message: message.value
+    });
+});
+
+socket.on('chat', function(data) {
+    output.innerHTML += "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
--- index.js
+++ index.js
@@ -12,4 +12,7 @@ let io = socket(server);
 
 io.on("connection", function(socket) {
     console.log('made socket connection.', socket.id);
+    socket.on('chat', function(data) {
+        io.sockets.emit('chat', data);
+    });
 });
